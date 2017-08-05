d35c083ead5aa93d1734f2e5d2713c1be17b629a
Leon Morley <lcmorley@gmail.com>
2017-08-05 11:31:18 +0100

    Amend the test class

--- BMBTApi-boot/src/test/java/uk/co/olimor/BMBTApi_boot/controller/BMBTControllerTest.java
+++ BMBTApi-boot/src/test/java/uk/co/olimor/BMBTApi_boot/controller/BMBTControllerTest.java
@@ -3,8 +3,9 @@ package uk.co.olimor.BMBTApi_boot.controller;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class BMBTControllerTest {
-	
+
 	@Test
 	public void testInit() {
 		Assert.assertNotNull(new BMBTController());
