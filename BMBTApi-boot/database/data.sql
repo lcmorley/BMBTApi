95d97f5ea326b37b574303cc5e9913b7fb5e9b99
Leon Morley <lcmorley@gmail.com>
2017-08-30 11:55:37 +0100

    Amend data script

--- BMBTApi-boot/database/data.sql
+++ BMBTApi-boot/database/data.sql
@@ -13,6 +13,9 @@ VALUES (4, '/');
 INSERT INTO test
 VALUES  (1, 'Reception', 20);
 
+INSERT INTO test
+VALUES (2, "Year 1", 30);
+
 INSERT INTO testQuestion 
 VALUES (1, 1, 4, 4, 1);
 
@@ -33,3 +36,54 @@ VALUES (6, 1, 3, 3, 1);
 
 INSERT INTO testQuestion 
 VALUES (7, 1, 2, 3, 1);
+
+INSERT INTO testQuestion
+VALUES (1, 2, 5, 5, 1);
+
+INSERT INTO testQuestion
+VALUES (2, 2, 9, 9, 1);
+
+INSERT INTO testQuestion
+VALUES (3, 2, 4, 2, 1);
+
+INSERT INTO testQuestion
+VALUES (4, 2, 6, 6, 1);
+
+INSERT INTO testQuestion
+VALUES (5, 2, 3, 7, 1);
+
+INSERT INTO testQuestion
+VALUES (6, 2, 8, 8, 1);
+
+INSERT INTO testQuestion
+VALUES (7, 2, 2, 8, 1);
+
+INSERT INTO testQuestion
+VALUES (8, 2, 5, 2, 1);
+
+INSERT INTO testQuestion
+VALUES (9, 2, 9, 2, 1);
+
+INSERT INTO testQuestion
+VALUES (10, 2, 4, 3, 1);
+
+INSERT INTO testQuestion
+VALUES (11, 2, 1, 9, 1);
+
+INSERT INTO testQuestion
+VALUES (12, 2, 7, 7, 1);
+
+INSERT INTO testQuestion
+VALUES (13, 2, 6, 3, 1);
+
+INSERT INTO testQuestion
+VALUES (14, 2, 4, 6, 1);
+
+INSERT INTO testQuestion
+VALUES (15, 2, 5, 3, 1);
+
+INSERT INTO testQuestion
+VALUES (16, 2, 7, 2, 1);
+
+INSERT INTO testQuestion
+VALUES (17, 2, 6, 3, 1);
