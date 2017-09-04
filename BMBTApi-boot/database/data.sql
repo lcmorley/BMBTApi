a90dda7e0a9bc670479fa91d8cc669804cd02575
Leon Morley <lcmorley@gmail.com>
2017-09-04 12:19:10 +0100

    changes to add another test

--- BMBTApi-boot/database/data.sql
+++ BMBTApi-boot/database/data.sql
@@ -16,6 +16,9 @@ VALUES  (1, 'Reception', 20);
 INSERT INTO test
 VALUES (2, "Year 1", 30);
 
+INSERT INTO test
+VALUES (3, "Total Recall 3", 90);
+
 INSERT INTO testQuestion 
 VALUES (1, 1, 4, 4, 1);
 
@@ -36,6 +39,11 @@ VALUES (6, 1, 3, 3, 1);
 
 INSERT INTO testQuestion 
 VALUES (7, 1, 2, 3, 1);
+
+
+
+
+
 
 INSERT INTO testQuestion
 VALUES (1, 2, 5, 5, 1);
@@ -87,3 +95,152 @@ VALUES (16, 2, 7, 2, 1);
 
 INSERT INTO testQuestion
 VALUES (17, 2, 6, 3, 1);
+
+
+
+
+INSERT INTO testQuestion
+VALUES (1, 3, 4, 9, 1);
+
+INSERT INTO testQuestion
+VALUES (2, 3, 3, 5, 3);
+
+INSERT INTO testQuestion
+VALUES (3, 3, 9, 2, 3);
+
+INSERT INTO testQuestion
+VALUES (4, 3, 6, 9, 1);
+
+INSERT INTO testQuestion
+VALUES (5, 3, 9, 5, 3);
+
+INSERT INTO testQuestion
+VALUES (6, 3, 2, 10, 3);
+
+INSERT INTO testQuestion
+VALUES (7, 3, 6, 2, 3);
+
+INSERT INTO testQuestion
+VALUES (8, 3, 5, 4, 1);
+
+INSERT INTO testQuestion
+VALUES (9,  3, 7, 5, 3);
+
+INSERT INTO testQuestion
+VALUES (10, 3, 7, 8, 1);
+
+INSERT INTO testQuestion
+VALUES (11, 3, 5, 5, 3);
+
+INSERT INTO testQuestion
+VALUES (12, 3, 5, 10, 3);
+
+INSERT INTO testQuestion
+VALUES (13, 3, 8, 2, 3);
+
+INSERT INTO testQuestion
+VALUES (14, 3, 7, 10, 3);
+
+INSERT INTO testQuestion
+VALUES (15, 3, 4, 2, 3);
+
+INSERT INTO testQuestion
+VALUES (16, 3, 3, 10, 3);
+
+INSERT INTO testQuestion
+VALUES (17, 3, 6, 7, 1);
+
+INSERT INTO testQuestion
+VALUES (18, 3, 8, 5, 3);
+
+INSERT INTO testQuestion
+VALUES (19, 3, 5, 9, 1);
+
+INSERT INTO testQuestion
+VALUES (20, 3, 2, 2, 3);
+
+INSERT INTO testQuestion
+VALUES (21, 3, 4, 10, 3);
+
+INSERT INTO testQuestion
+VALUES (22, 3, 7, 9, 1);
+
+INSERT INTO testQuestion
+VALUES (23, 3, 3, 2, 3);
+
+INSERT INTO testQuestion
+VALUES (24, 3, 3, 8, 1);
+
+INSERT INTO testQuestion
+VALUES (25, 3, 8, 9, 1);
+
+INSERT INTO testQuestion
+VALUES (26, 3, 9, 10, 3);
+
+INSERT INTO testQuestion
+VALUES (27, 3, 3, 9, 1);
+
+INSERT INTO testQuestion
+VALUES (28, 3, 2, 5, 3);
+
+INSERT INTO testQuestion
+VALUES (29, 3, 7, 2, 3);
+
+INSERT INTO testQuestion
+VALUES (30, 3, 5, 2, 3);
+
+INSERT INTO testQuestion
+VALUES (31, 3, 5, 6, 1);
+
+INSERT INTO testQuestion
+VALUES (32, 3, 4, 7, 1);
+
+INSERT INTO testQuestion
+VALUES (33, 3, 6, 10, 3);
+
+INSERT INTO testQuestion
+VALUES (34, 3, 5, 7, 1);
+
+INSERT INTO testQuestion
+VALUES (35, 3, 4, 5, 3);
+
+INSERT INTO testQuestion
+VALUES (36, 3, 6, 8, 1);
+
+INSERT INTO testQuestion
+VALUES (37, 3, 8, 10, 3);
+
+INSERT INTO testQuestion
+VALUES (38, 3, 4, 8, 1);
+
+INSERT INTO testQuestion
+VALUES (39, 3, 5, 8, 1);
+
+INSERT INTO testQuestion
+VALUES (40, 3, 8, 10, 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
