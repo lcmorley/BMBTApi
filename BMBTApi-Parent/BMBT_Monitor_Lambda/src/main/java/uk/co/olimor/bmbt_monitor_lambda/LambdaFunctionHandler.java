35e7bdf3bb4879bbfbab66627af74aa219455fce
Leon Morley <lcmorley@gmail.com>
2017-10-05 14:19:16 +0100

    Add aws sns integration into BMBT monitor.

--- BMBTApi-Parent/BMBT_Monitor_Lambda/src/main/java/uk/co/olimor/bmbt_monitor_lambda/LambdaFunctionHandler.java
+++ BMBTApi-Parent/BMBT_Monitor_Lambda/src/main/java/uk/co/olimor/bmbt_monitor_lambda/LambdaFunctionHandler.java
@@ -2,8 +2,13 @@ package uk.co.olimor.bmbt_monitor_lambda;
 
 import javax.sql.DataSource;
 
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
@@ -25,9 +30,16 @@ public class LambdaFunctionHandler implements RequestHandler<MonitorInput, Strin
 			final JWTUtil util = new JWTUtil();
 			util.setSecret(monitorInput.getSecret());
 
-			return new BMBTApiIntegrationTest().runTest(monitorInput.getContextRoot(), getDataSource(monitorInput, context),
-					util, context.getLogger(), monitorInput.getAppUserName(), monitorInput.getAppPassword()) 
-					? "Passed" : "Failed";
+			final boolean result = new BMBTApiIntegrationTest().runTest(monitorInput.getContextRoot(), getDataSource(monitorInput, context),
+					util, context.getLogger(), monitorInput.getAppUserName(), monitorInput.getAppPassword());
+			
+			if (!result) {
+				//create a new SNS client and set endpoint
+				AmazonSNS snsClient = AmazonSNSClient.builder().withRegion(Regions.US_EAST_1).build();			
+				snsClient.publish("arn:aws:sns:us-east-1:981596309454:BMBTMonitorState", "BMBT Monitor failed.");
+			} 
+			
+			return result ? "Passed" : "Failed";
 
 		} catch (final Exception e) {
 			context.getLogger().log("An exception occurred whilst attempting to convert Json to Object." 
